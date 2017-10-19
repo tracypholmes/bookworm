052f5679aa1f143813c72ab43ed976dd651e69ba
tracypholmes <tracypholmes@gmail.com>
2017-10-18 22:13:49 -0500

    updated with created action and strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,19 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def product_params
+    params.require(:product).permit(:title, :author, :description, :isbn_13, :image_url, :price)
+  end
 end
