9afa1bfc9ae8330420659d89a03c6dd613a2c351
Tracy P Holmes <tracypholmes@gmail.com>
2017-10-20 17:49:14 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171019023021) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
