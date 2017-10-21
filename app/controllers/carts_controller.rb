6fec106fa51765dd84dc0867b3e889b42d1cf3df
tracy p holmes <tracypholmes@gmail.com>
2017-10-20 22:56:56 -0500

    Built out carts & line items controllers w/CRUD

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,48 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @carts = Cart.all
+  end
+
+  def show
+  end
+
+  def new
+    @cart = Cart.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+    if @cart.save
+      redirect_to @cart, notice: 'Cart was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @cart.update(cart_params)
+      redirect_to @cart, notice: 'Cart was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cart.destroy
+    redirect_to carts_url, notice: 'Cart was successfully destroyed.'
+  end
+
+  private
+    def set_cart
+      @cart = Cart.find(params[:id])
+    end
+
+    def cart_params
+      params.fetch(:cart, {})
+    end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,57 @@
 class LineItemsController < ApplicationController
+  include CurrentCart # don't forget the CurrentCart module!
+  
+  before_action :set_cart, only: [:create]
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @line_items = LineItem.all
+  end
+
+  def show
+  end
+
+  def new
+    @line_item = LineItem.new
+  end
+
+  def edit
+  end
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
+    if @line_item.save
+      redirect_to @line_item.cart,
+        notice: 'Line item was successfully created.'
+    else
+      render :new 
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @line_item.update(line_item_params)
+        redirect_to @line_item, notice: 'Line item was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      redirect_to line_items_url, notice: 'Line item was successfully destroyed.'
+    end
+  end
+
+  private
+    def set_line_item
+      @line_item = LineItem.find(params[:id])
+    end
+
+    def line_item_params
+      params.require(:line_item).permit(:product_id, :cart_id)
+    end
 end
