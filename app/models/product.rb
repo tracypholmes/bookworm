934aef923cbde6ae268271948729a59da3476b6a
tracy p holmes <tracypholmes@gmail.com>
2017-10-20 19:30:40 -0500

    began adding validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :author, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
 end
