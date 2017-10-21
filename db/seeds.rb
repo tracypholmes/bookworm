9307e21a39dfc0a0bd9a9a1d16007e7dac454dfb
tracy p holmes <tracypholmes@gmail.com>
2017-10-20 20:13:03 -0500

    Updated seed data with paragraph description

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Product.destroy_all
 25.times do |index|
   Product.create!(title: Faker::Book.unique.title,
                   author: Faker::Book.author,
-                  description: Faker::Lorem.sentence(7, false, 0).chop,
+                  description: Faker::Hipster.paragraph,
                   isbn_13: Faker::Number.unique.number(13),
                   image_url: Faker::Placeholdit.image("60x70"),
                   price: Faker::Number.decimal(2)
